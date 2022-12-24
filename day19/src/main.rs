3047c6d34f1b44d1723de0eda15799ed9cd527b2
ColeTrammer <cole.trammer@gmail.com>
2022-12-23 18:08:49 -0800

    Solve day 19 part 2
    This approach is still inherently backtracking, but it now prunes
    useless nodes.
    
    The useless nodes identified are nodes where nothing is purchased
    or nodes where even if we built a new geode robot every turn, by
    the end, we wouldn't improve over the current highest value.
    
    This was enough to solve part b in only 15 seconds on my 5 year
    old laptop.

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,9 +1,139 @@
+#![feature(int_roundings)]
+
 use clap::Parser;
 use itertools::Itertools;
-use std::collections::{BTreeMap, BinaryHeap, HashSet};
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::read_to_string;
 use std::path::PathBuf;
+
+fn eval(
+    time: i32,
+    res: (i32, i32, i32, i32),
+    income: (i32, i32, i32, i32),
+    blueprint: (i32, i32, (i32, i32), (i32, i32)),
+    r: &mut i32,
+    cache: &mut HashMap<(i32, (i32, i32, i32, i32), (i32, i32, i32, i32)), i32>,
+) -> i32 {
+    if time < 0 {
+        return 0;
+    } else if time == 0 {
+        *r = res.3;
+        return res.3;
+    } else if let Some(result) = cache.get(&(time, res, income)) {
+        return *result;
+    }
+
+    let (ore, clay, (obs_ore, obs_clay), (geode_ore, geode_obs)) = blueprint;
+
+    let mut best = res.3 + income.3 * time;
+
+    // Even if we kept making new geode robots every turn, we
+    // still wouldn't surpass the highest value seen so far, so
+    // just bail.
+    if best + (time - 1) * time / 2 <= *r {
+        return 0;
+    }
+
+    fn time_until(current: i32, income: i32, max_t: i32, needed: i32) -> i32 {
+        if current >= needed {
+            return 0;
+        }
+        if income == 0 {
+            return max_t;
+        }
+        // current + income * t >= needed
+        // income * t >= needed - current
+        // t >= (needed - current) / income
+        // t = ceil((needed - current) / income)
+        (needed - current).div_ceil(income)
+    }
+
+    // Try making ore.
+    {
+        let time_wait = time_until(res.0, income.0, time, ore);
+        let new_res = (
+            res.0 + income.0 * (time_wait + 1) - ore,
+            res.1 + income.1 * (time_wait + 1),
+            res.2 + income.2 * (time_wait + 1),
+            res.3 + income.3 * (time_wait + 1),
+        );
+        let new_income = (income.0 + 1, income.1, income.2, income.3);
+        best = best.max(eval(
+            time - time_wait - 1,
+            new_res,
+            new_income,
+            blueprint,
+            r,
+            cache,
+        ));
+    }
+
+    // Try making clay.
+    {
+        let time_wait = time_until(res.0, income.0, time, clay);
+        let new_res = (
+            res.0 + income.0 * (time_wait + 1) - clay,
+            res.1 + income.1 * (time_wait + 1),
+            res.2 + income.2 * (time_wait + 1),
+            res.3 + income.3 * (time_wait + 1),
+        );
+        let new_income = (income.0, income.1 + 1, income.2, income.3);
+        best = best.max(eval(
+            time - time_wait - 1,
+            new_res,
+            new_income,
+            blueprint,
+            r,
+            cache,
+        ));
+    }
+
+    // Try making obs.
+    {
+        let time_wait = time_until(res.0, income.0, time, obs_ore)
+            .max(time_until(res.1, income.1, time, obs_clay));
+        let new_res = (
+            res.0 + income.0 * (time_wait + 1) - obs_ore,
+            res.1 + income.1 * (time_wait + 1) - obs_clay,
+            res.2 + income.2 * (time_wait + 1),
+            res.3 + income.3 * (time_wait + 1),
+        );
+        let new_income = (income.0, income.1, income.2 + 1, income.3);
+        best = best.max(eval(
+            time - time_wait - 1,
+            new_res,
+            new_income,
+            blueprint,
+            r,
+            cache,
+        ));
+    }
+
+    // Try making geo.
+    {
+        let time_wait = time_until(res.0, income.0, time, geode_ore)
+            .max(time_until(res.2, income.2, time, geode_obs));
+        let new_res = (
+            res.0 + income.0 * (time_wait + 1) - geode_ore,
+            res.1 + income.1 * (time_wait + 1),
+            res.2 + income.2 * (time_wait + 1) - geode_obs,
+            res.3 + income.3 * (time_wait + 1),
+        );
+        let new_income = (income.0, income.1, income.2, income.3 + 1);
+        best = best.max(eval(
+            time - time_wait - 1,
+            new_res,
+            new_income,
+            blueprint,
+            r,
+            cache,
+        ));
+    }
+
+    cache.insert((time, res, income), best);
+    best
+}
 
 fn part_a(input: String) -> i32 {
     let mut blueprints = vec![];
@@ -23,83 +153,17 @@ fn part_a(input: String) -> i32 {
     let mut result = 0;
 
     for (ind, blueprint) in blueprints.iter().enumerate() {
-        fn eval(
-            time: i32,
-            res: (i32, i32, i32, i32),
-            income: (i32, i32, i32, i32),
-            blueprint: (i32, i32, (i32, i32), (i32, i32)),
-            r: i32,
-            cache: &mut BTreeMap<(i32, (i32, i32, i32, i32), (i32, i32, i32, i32), i32), i32>,
-        ) -> i32 {
-            if time <= 0 {
-                return res.3;
-            }
+        let mut cache = HashMap::new();
 
-            if let Some(result) = cache.get(&(time, res, income, r)) {
-                return *result;
-            }
-
-            let (ore, clay, (obs_ore, obs_clay), (geode_ore, geode_obs)) = blueprint;
-
-            let new_res = (
-                res.0 + income.0,
-                res.1 + income.1,
-                res.2 + income.2,
-                res.3 + income.3,
-            );
-
-            let mut best = eval(time - 1, new_res, income, blueprint, r, cache);
-
-            // Can build ore
-            if res.0 >= ore && r <= 4 {
-                let mut new_income = income;
-                new_income.0 += 1;
-
-                let mut new_res = new_res;
-                new_res.0 -= ore;
-
-                best = best.max(eval(time - 1, new_res, new_income, blueprint, 4, cache));
-            }
-
-            // Can build clay
-            if res.0 >= clay && r <= 4 {
-                let mut new_income = income;
-                new_income.1 += 1;
-
-                let mut new_res = new_res;
-                new_res.0 -= clay;
-                best = best.max(eval(time - 1, new_res, new_income, blueprint, 4, cache));
-            }
-
-            // Can build obs
-            if res.0 >= obs_ore && res.1 >= obs_clay && r <= 4 {
-                let mut new_income = income;
-                new_income.2 += 1;
-
-                let mut new_res = new_res;
-                new_res.0 -= obs_ore;
-                new_res.1 -= obs_clay;
-                best = best.max(eval(time - 1, new_res, new_income, blueprint, 4, cache));
-            }
-
-            // Can build geo
-            if res.0 >= geode_ore && res.2 >= geode_obs && r <= 4 {
-                let mut new_income = income;
-                new_income.3 += 1;
-
-                let mut new_res = new_res;
-                new_res.0 -= geode_ore;
-                new_res.2 -= geode_obs;
-                best = best.max(eval(time - 1, new_res, new_income, blueprint, 4, cache));
-            }
-
-            cache.insert((time, res, income, r), best);
-            return best;
-        }
-
-        let mut cache = BTreeMap::new();
-
-        let score = eval(24, (0, 0, 0, 0), (1, 0, 0, 0), *blueprint, 4, &mut cache);
+        let mut r = 0;
+        let score = eval(
+            24,
+            (0, 0, 0, 0),
+            (1, 0, 0, 0),
+            *blueprint,
+            &mut r,
+            &mut cache,
+        );
 
         println!("{}: {}", ind + 1, score);
 
@@ -107,36 +171,6 @@ fn part_a(input: String) -> i32 {
     }
 
     result
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Ord, Hash)]
-struct Node {
-    time: i32,
-    res: (i32, i32, i32, i32),
-    income: (i32, i32, i32, i32),
-}
-
-impl Node {
-    fn score(self) -> i128 {
-        // let a = self.income.3 * self.time + self.res.3;
-        // let b = self.income.2 * self.time + self.res.2;
-        // let c = self.income.1 * self.time + self.res.1;
-        // let d = self.income.0 * self.time + self.res.0;
-
-        // let a = a as i128;
-        // let b = b as i128;
-        // let c = c as i128;
-        // let d = d as i128;
-
-        // -((a << 96) | (b << 64) | (c << 32) | d)
-        -self.time as i128
-    }
-}
-
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.score().partial_cmp(&other.score())
-    }
 }
 
 fn part_b(input: String) -> i64 {
@@ -157,253 +191,17 @@ fn part_b(input: String) -> i64 {
     let mut result = 1;
 
     for (ind, blueprint) in blueprints.iter().take(3).enumerate() {
-        // let mut queue = BinaryHeap::new();
-        // let mut score = 0;
+        let mut cache = HashMap::new();
 
-        // let mut visited = HashSet::new();
-
-        // queue.push(Node {
-        //     time: 32,
-        //     res: (0, 0, 0, 0),
-        //     income: (1, 0, 0, 0),
-        // });
-        // visited.insert(Node {
-        //     time: 32,
-        //     res: (0, 0, 0, 0),
-        //     income: (1, 0, 0, 0),
-        // });
-
-        // while !queue.is_empty() {
-        //     let Node { time, res, income } = queue.pop().unwrap();
-
-        //     // println!("time: {time}, res: {res:?}, income: {income:?}");
-
-        //     if time == 0 {
-        //         // score = res.3;
-        //         // break;
-        //         let old_score = score;
-        //         score = score.max(res.3);
-        //         if score > old_score {
-        //             println!("got new score: {}", score);
-        //             continue;
-        //         } else if score == old_score {
-        //             continue;
-        //         } else {
-        //             println!("got old score {} - {}", score, res.3);
-        //             continue;
-        //         }
-        //     }
-
-        //     let (ore, clay, (obs_ore, obs_clay), (geode_ore, geode_obs)) = blueprint.clone();
-
-        //     let new_res = (
-        //         res.0 + income.0,
-        //         res.1 + income.1,
-        //         res.2 + income.2,
-        //         res.3 + income.3,
-        //     );
-
-        //     if !visited.contains(&Node {
-        //         time: time - 1,
-        //         res: new_res,
-        //         income,
-        //     }) {
-        //         queue.push(Node {
-        //             time: time - 1,
-        //             res: new_res,
-        //             income,
-        //         });
-        //         visited.insert(Node {
-        //             time: time - 1,
-        //             res: new_res,
-        //             income,
-        //         });
-        //     }
-
-        //     // Can build ore
-        //     if res.0 >= ore {
-        //         let mut new_income = income;
-        //         new_income.0 += 1;
-
-        //         let mut new_res = new_res;
-        //         new_res.0 -= ore;
-
-        //         if !visited.contains(&Node {
-        //             time: time - 1,
-        //             res: new_res,
-        //             income: new_income,
-        //         }) {
-        //             queue.push(Node {
-        //                 time: time - 1,
-        //                 res: new_res,
-        //                 income: new_income,
-        //             });
-        //             visited.insert(Node {
-        //                 time: time - 1,
-        //                 res: new_res,
-        //                 income: new_income,
-        //             });
-        //         }
-        //     }
-
-        //     // Can build clay
-        //     if res.0 >= clay {
-        //         let mut new_income = income;
-        //         new_income.1 += 1;
-
-        //         let mut new_res = new_res;
-        //         new_res.0 -= clay;
-
-        //         if !visited.contains(&Node {
-        //             time: time - 1,
-        //             res: new_res,
-        //             income: new_income,
-        //         }) {
-        //             queue.push(Node {
-        //                 time: time - 1,
-        //                 res: new_res,
-        //                 income: new_income,
-        //             });
-        //             visited.insert(Node {
-        //                 time: time - 1,
-        //                 res: new_res,
-        //                 income: new_income,
-        //             });
-        //         }
-        //     }
-
-        //     // Can build obs
-        //     if res.0 >= obs_ore && res.1 >= obs_clay {
-        //         let mut new_income = income;
-        //         new_income.2 += 1;
-
-        //         let mut new_res = new_res;
-        //         new_res.0 -= obs_ore;
-        //         new_res.1 -= obs_clay;
-
-        //         if !visited.contains(&Node {
-        //             time: time - 1,
-        //             res: new_res,
-        //             income: new_income,
-        //         }) {
-        //             queue.push(Node {
-        //                 time: time - 1,
-        //                 res: new_res,
-        //                 income: new_income,
-        //             });
-        //             visited.insert(Node {
-        //                 time: time - 1,
-        //                 res: new_res,
-        //                 income: new_income,
-        //             });
-        //         }
-        //     }
-
-        //     // Can build geo
-        //     if res.0 >= geode_ore && res.2 >= geode_obs {
-        //         let mut new_income = income;
-        //         new_income.3 += 1;
-
-        //         let mut new_res = new_res;
-        //         new_res.0 -= geode_ore;
-        //         new_res.2 -= geode_obs;
-
-        //         if !visited.contains(&Node {
-        //             time: time - 1,
-        //             res: new_res,
-        //             income: new_income,
-        //         }) {
-        //             queue.push(Node {
-        //                 time: time - 1,
-        //                 res: new_res,
-        //                 income: new_income,
-        //             });
-        //             visited.insert(Node {
-        //                 time: time - 1,
-        //                 res: new_res,
-        //                 income: new_income,
-        //             });
-        //         }
-        //     }
-        // }
-
-        fn eval(
-            time: i32,
-            res: (i32, i32, i32, i32),
-            income: (i32, i32, i32, i32),
-            blueprint: (i32, i32, (i32, i32), (i32, i32)),
-            r: i32,
-            cache: &mut BTreeMap<(i32, (i32, i32, i32, i32), (i32, i32, i32, i32), i32), i32>,
-        ) -> i32 {
-            if time <= 0 {
-                return res.3;
-            }
-
-            // if let Some(result) = cache.get(&(time, res, income, r)) {
-            // return *result;
-            // }
-
-            let (ore, clay, (obs_ore, obs_clay), (geode_ore, geode_obs)) = blueprint;
-
-            let new_res = (
-                res.0 + income.0,
-                res.1 + income.1,
-                res.2 + income.2,
-                res.3 + income.3,
-            );
-
-            let mut best = eval(time - 1, new_res, income, blueprint, r, cache);
-
-            // Can build ore
-            if res.0 >= ore && r <= 4 {
-                let mut new_income = income;
-                new_income.0 += 1;
-
-                let mut new_res = new_res;
-                new_res.0 -= ore;
-
-                best = best.max(eval(time - 1, new_res, new_income, blueprint, 4, cache));
-            }
-
-            // Can build clay
-            if res.0 >= clay && r <= 4 {
-                let mut new_income = income;
-                new_income.1 += 1;
-
-                let mut new_res = new_res;
-                new_res.0 -= clay;
-                best = best.max(eval(time - 1, new_res, new_income, blueprint, 4, cache));
-            }
-
-            // Can build obs
-            if res.0 >= obs_ore && res.1 >= obs_clay && r <= 4 {
-                let mut new_income = income;
-                new_income.2 += 1;
-
-                let mut new_res = new_res;
-                new_res.0 -= obs_ore;
-                new_res.1 -= obs_clay;
-                best = best.max(eval(time - 1, new_res, new_income, blueprint, 4, cache));
-            }
-
-            // Can build geo
-            if res.0 >= geode_ore && res.2 >= geode_obs && r <= 4 {
-                let mut new_income = income;
-                new_income.3 += 1;
-
-                let mut new_res = new_res;
-                new_res.0 -= geode_ore;
-                new_res.2 -= geode_obs;
-                best = best.max(eval(time - 1, new_res, new_income, blueprint, 4, cache));
-            }
-
-            // cache.insert((time, res, income, r), best);
-            return best;
-        }
-
-        let mut cache = BTreeMap::new();
-
-        let score = eval(32, (0, 0, 0, 0), (1, 0, 0, 0), *blueprint, 4, &mut cache);
+        let mut r = 0;
+        let score = eval(
+            32,
+            (0, 0, 0, 0),
+            (1, 0, 0, 0),
+            *blueprint,
+            &mut r,
+            &mut cache,
+        );
 
         println!("{}: {}", ind + 1, score);
 
